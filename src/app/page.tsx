09935885d2eff9dedfeeb4e0ead7805f9be54ca7
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2024-12-26 21:37:18 -0600

    fix: error applicattion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import HeroSection from './components/sections/HeroSection'
-import SolutionsEmpresasSection from './components/sections/SolutionsEmpresasSection'
-import SolutionsUsuariosSection from './components/sections/SolutionsUsuariosSection'
-import TestimoniosSection from './components/sections/TestimoniosSection'
-import CTASection from './components/sections/CTASection'
+import dynamic from 'next/dynamic'
+
+const HeroSection = dynamic(() => import('./components/sections/HeroSection'), { ssr: false })
+const SolutionsEmpresasSection = dynamic(() => import('./components/sections/SolutionsEmpresasSection'), { ssr: false })
+const SolutionsUsuariosSection = dynamic(() => import('./components/sections/SolutionsUsuariosSection'), { ssr: false })
+const TestimoniosSection = dynamic(() => import('./components/sections/TestimoniosSection'), { ssr: false })
+const CTASection = dynamic(() => import('./components/sections/CTASection'), { ssr: false })
 
 export default function Home() {
   return (
