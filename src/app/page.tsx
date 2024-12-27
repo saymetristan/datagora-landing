1cec560c36ca6599222b0de5186fef4d14b1c635
Jorge Tristan <Jorge-tristan@Josues-MacBook-Air.local>
2024-12-26 21:34:35 -0600

    fix: double dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import DynamicCTASection from './components/sections/DynamicCTASection'
+import HeroSection from './components/sections/HeroSection'
 import SolutionsEmpresasSection from './components/sections/SolutionsEmpresasSection'
 import SolutionsUsuariosSection from './components/sections/SolutionsUsuariosSection'
 import TestimoniosSection from './components/sections/TestimoniosSection'
+import CTASection from './components/sections/CTASection'
 
 export default function Home() {
   return (
     <main>
-      <DynamicCTASection />
+      <HeroSection />
       <SolutionsEmpresasSection />
       <SolutionsUsuariosSection />
       <TestimoniosSection />
+      <CTASection />
     </main>
   )
 }
